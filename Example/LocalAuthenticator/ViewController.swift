42da5a408ead67ff5fb3fc685a097ecdb45ee6bd
Junaid Rehmat <junaid.rehmat.rana@gmail.com>
2020-04-28 12:59:43 +0500

    Initial commit

---
+++ Example/LocalAuthenticator/ViewController.swift
@@ -0,0 +1,24 @@
+//
+//  ViewController.swift
+//  LocalAuthenticator
+//
+//  Created by MrJai on 04/28/2020.
+//  Copyright (c) 2020 MrJai. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+}
+
